353b48ba89992bc3a1b3d738df9880eb1d389e4c
MC Simoes <mariacristina.simoes@gmail.com>
2018-12-30 19:59:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,7 @@
-def convert(phrase)
-arr_phrase = phrase.split
-end
-def reverse_each_word(phrase, arr_phrase)
-convert(phrase)
-arr_phrase.collect do |word|
+def reverse_each_word(phrase)
+phrase.split
+phrase.collect do |word|
 word.reverse
 end
-array.join(" ")
-end 
+phrase.join(" ")
+end
